d08ac4d6d6cdb8ef906b7e0dc10701cd7c2ac70a
Arnar Freyr <arnar-freyr@hotmail.com>
2016-12-05 20:01:03 +0000

    rmContainers and rmImages added for ease of testing

---
+++ scripts/removeContainers.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo docker stop $(sudo docker ps -qa) #inner function selects all containers, the outer stops them
+sudo docker rm $(sudo docker ps -qa) #inner function selects all containers, the outer removes them
