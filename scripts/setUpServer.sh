110a7608dfc3ace91b2268155ac894d8971a2d63
Arnar Freyr <arnar-freyr@hotmail.com>
2016-12-06 17:25:57 +0000

    added further steps in setting up docker+git at the server instance

--- scripts/setUpServer.sh
+++ scripts/setUpServer.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
-  sudo yum update -y #checks for updates on the instance
-  sudo yum install -y docker # installs docker on the instance
-  sudo service docker start #starts docker on the instance
-  sudo yum install -y git-all
+  sudo yum update -y #checks for updates on the instance server
+  sudo yum install -y docker # installs docker on the instance server
+  sudo service docker start #starts docker on the instance server
+  sudo yum install -y git-all #installs git on the instance server
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose # fetches docker-compose and sets it up on the server
+  sudo chmod +x /usr/local/bin/docker-compose #allows execution on docker-compose
 exit 0
