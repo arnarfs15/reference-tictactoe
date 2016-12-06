1b4feb7f58fdaf676bf8d4445e48931b0dff0727
Arnar Freyr <arnar-freyr@hotmail.com>
2016-12-06 18:33:40 +0000

    finished commenting scripts

--- scripts/loginAWS.sh
+++ scripts/loginAWS.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -i arnarfs15.pem ec2-user@52.208.158.45
+ssh -i arnarfs15.pem ec2-user@52.208.158.45 #logs in to amazon web services using key pair arnarfs15
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -e
+set -e #if any operation fails, exits
 
-sleep 10
-npm run migratedb
+sleep 10 #needs to wait for some time to load, needs to be done better
+npm run migratedb #migrates the database
 node run.js
 
 exit 0
