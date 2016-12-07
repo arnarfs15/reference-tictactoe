63f3b473ec18c322fc80a877ffa5a5e50c9b201e
Arnar Freyr <arnar-freyr@hotmail.com>
2016-12-07 16:36:19 +0000

    added the jenkinsBuild script which jenkins runs on build

---
+++ scripts/jenkinsBuild.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+  git clean -dfx        #cleans the directory
+  git stash             #stashes all current git changes
+  rm -rf node_modules   #removes the node modules file and all files within
+  npm install           #installs npm at project root
+  cd client/
+  rm -rf node_modules   #removes the node_modules file and all files within from client
+  npm install           #installs npm in client
+  cd ..
+  ./scripts/package.sh  #runs the package script
+exit 0
