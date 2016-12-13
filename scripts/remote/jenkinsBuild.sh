7d467dbc1a5b80ffba4202ff11ca97ea773384ec
arnarfs15 <arnar-freyr@hotmail.com>
2016-12-13 15:42:31 +0000

    final final fix

--- scripts/remote/jenkinsBuild.sh
+++ scripts/remote/jenkinsBuild.sh
@@ -10,6 +10,9 @@ set -e
   npm install           #installs npm in client
   cd ..
   ./scripts/package.sh  #runs the package script
-  scp -o StrictHostKeyChecking=no "../arnarfs15.pem" ./docker-compose.yaml ec2-user@52.208.158.45:~/docker-compose.yaml
-  scp -o StrictHostKeyChecking=no "../arnarfs15.pem" ./build/.env ec2-user@52.208.158.45:~/.env 
+
+  echo 'Copying files to AWS'
+
+  scp -o StrictHostKeyChecking=no -i "../arnarfs15.pem" ./docker-compose.yaml ec2-user@52.208.158.45:~/docker-compose.yaml
+  scp -o StrictHostKeyChecking=no -i "../arnarfs15.pem" ./build/.env ec2-user@52.208.158.45:~/.env
 exit 0
