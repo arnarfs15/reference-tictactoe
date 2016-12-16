cfaf06177fbf9e48f896e31a9cd5731aeee41378
arnarfs15 <arnar-freyr@hotmail.com>
2016-12-16 19:02:01 +0000

    acceptance test stage test

--- client/src/routing-context.js
+++ client/src/routing-context.js
@@ -15,7 +15,7 @@ function routingContext(injected){
     const environment = injected('env');
     var socketURI;
     if(environment==='development' || environment==='test'){
-        socketURI='http://localhost:8080'
+        socketURI='http://52.208.158.45/'
     } else {
         socketURI='http://52.208.158.45/'
     }
