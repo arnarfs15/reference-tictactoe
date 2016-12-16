a8a9912d10a264feb469b72767d30cc4589a88b4
arnarfs15 <arnar-freyr@hotmail.com>
2016-12-16 18:17:47 +0000

    new Jenkins test for acceptance testing

--- scripts/remote/jenkinsBuild.sh
+++ scripts/remote/jenkinsBuild.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 set -e
-  git clean -dfx        #cleans the directory
-  git stash             #stashes all current git changes
-  rm -rf node_modules   #removes the node modules file and all files within
-  npm install           #installs npm at project root
-  cd client/
-  rm -rf node_modules   #removes the node_modules file and all files within from client
-  npm install           #installs npm in client
-  cd ..
+  ,/scripts/remote/npm-install
   ./scripts/package.sh  #runs the package script
 
   echo 'Running tests and making XML files'
