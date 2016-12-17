0be6b23182b7e66df36a3449e4e58b10bac17e29
arnarfs15 <arnar-freyr@hotmail.com>
2016-12-17 17:54:41 +0000

    added dockercompose.sh script to load test to deploy a fresh instance

--- scripts/remote/jenkinsLoadTest.sh
+++ scripts/remote/jenkinsLoadTest.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-./scripts/remote/npm-install.sh
-npm run loadtest
+./scripts/remote/npm-install.sh       #install npm for the load stage
+npm run loadtest                      #runs the test
+ssh -o StrictHostKeyChecking=no -i "../arnarfs15.pem" ec2-user@52.208.158.45 "./dockerCompose.sh"   #temporary if there is time a provisioning script will
+                                                                                                    #run a new instance before 
